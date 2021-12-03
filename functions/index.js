7b6b8771a8a744d2a3d686b863fb3335368f6c7f
danielsheetrit <danielshitrit022@gmail.com>
2021-12-03 15:30:58 +0200

    login implementation

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
-const serviceAccount = require('./service-account.json')
+const { isEmail, isEmpty } = require('./util');
+const serviceAccount = require('./service-account.json');
+const webApiCredential = require('./web-key-credential.json');
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const app = require('express')();
+
+const axios = require('axios');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -12,7 +17,6 @@ const db = admin.firestore();
 app.get('/screams', async (req, res) => {
 
     try {
-
         const snapshot =
             await db
                 .collection('screams')
@@ -25,11 +29,9 @@ app.get('/screams', async (req, res) => {
             ...doc.data()
         }))
 
-        return res.status(200).send(screams)
-
+        return res.status(200).json(screams)
     } catch (e) {
-
-        return res.status(500).send(e.message)
+        return res.status(500).json({ message: e.message })
     }
 })
 
@@ -39,38 +41,55 @@ app.post('/scream', async (req, res) => {
         body: req.body.body,
         userName: req.body.userName,
         createdAt: Date.now()
-    };
+    }
 
     try {
-
         const snapshot =
             await db
                 .collection('screams')
                 .add(screamToAdd)
 
-        return res.status(200).send(`Scream (id: ${snapshot.id}) added successfully`)
-
+        return res
+            .status(200)
+            .json({ message: `Scream (id: ${snapshot.id}) added successfully` })
     } catch (e) {
-
-        return res.status(500).send(e.message)
+        return res
+            .status(500)
+            .send({ message: e.message })
     }
 })
 
-//signup route
-
+//auth routes
 app.post('/signup', async (req, res) => {
 
-    const userToAdd = {
+    let userToAdd = {
         email: req.body.email,
         password: req.body.password,
-        userName: req.body.userName,
-        createdAt: Date.now()
+        confirmPassword: req.body.confirmPassword,
+        userName: req.body.userName
+    };
+
+    //Data validation
+    let errors = {};
+
+    if (isEmpty(userToAdd.email)) errors.email = 'Must not be empty.'
+    else if (!isEmail(userToAdd.email)) {
+        errors.email = 'Must be a valid email adress.'
     }
 
-    //TODO validate data
+    if (isEmpty(userToAdd.password)) errors.password = 'Must not be empty.'
+    else if (userToAdd.password !== userToAdd.confirmPassword) {
+        errors.confirmPassword = 'Passwords must match.'
+    }
+
+    if (isEmpty(userToAdd.userName)) errors.userName = 'Must not be empty.'
+
+    if (Object.keys(errors).length !== 0) {
+        return res.status(400).json(errors)
+    }
 
     try {
-
+        //cheking if username is already in use
         const userRecord =
             await db
                 .collection('users')
@@ -78,31 +97,64 @@ app.post('/signup', async (req, res) => {
                 .get()
 
         if (!userRecord.empty) {
-            return res.status(403).send('Username already in use')
+            return res.status(403).json({ message: 'Username already in use' })
         }
 
-        const snapshotAuth = await admin.auth().createUser({
+        //creating new user
+        const snapshot = await admin.auth().createUser({
             email: userToAdd.email,
             password: userToAdd.password
         })
 
+        //getting id token
         const token =
             await admin
                 .auth()
-                .createCustomToken(snapshotAuth.uid)
+                .createCustomToken(snapshot.uid)
 
-        return res.status(201).json({
-            message: `User (id:${snapshotAuth.uid}) added successfully`,
-            token: token
-        })
+        //Adding user to Database
+        userToAdd.userId = snapshot.uid
+        userToAdd.createdAt = Date.now()
+        await db
+            .collection('users')
+            .doc()
+            .set(userToAdd)
 
+        //sending token back to client 
+        return res.status(201).json({ token })
     } catch (e) {
-
         if (e.code === 'auth/email-already-exists') {
-            return res.status(403).send('Email already in use')
+            return res.status(403).json({ message: 'Email already in use' })
         } else {
-            return res.status(500).send(e.message)
+            return res.status(500).json({ message: e.message })
         }
+    }
+})
+
+app.post('/login', async (req, res) => {
+
+    const userToAuth = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    let errors = {}
+
+    if (isEmpty(userToAuth.email)) errors.email = 'Must not be empty.'
+    if (isEmpty(userToAuth.password)) errors.password = 'Must not be empty.'
+
+    if (Object.keys(errors).length !== 0) {
+        return res.status(400).json(errors)
+    }
+
+    try {
+        const authResult = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${webApiCredential.key}`, userToAuth)
+
+        const token = authResult.data.idToken
+
+        return res.status(200).json({ token })
+    } catch (e) {
+        return res.status(500).json({ message: e.message })
     }
 })
 
@@ -110,4 +162,4 @@ exports.api =
     functions
         .region('europe-central2')
         .https
-        .onRequest(app)
+        .onRequest(app);
